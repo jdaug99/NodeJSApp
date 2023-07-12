14e12dfe48904e4acfd850710d91b7f67e15dba6
jdaug99 <jdaug99@gmail.com>
2023-07-11 21:17:09 -0500

    Added POST create product, GET products, and GET prodcut by ID

--- server.js
+++ server.js
@@ -1,13 +1,50 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/productModel')
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-    res.send('Hello NODE API')
+    res.send('Hello Booster API')
 })
 
 app.get('/blog', (req, res) => {
     res.send('Hello blog!!')
+})
+
+app.get('/products', async(req, res) => {
+    try {
+
+        const products = await Product.find({})
+        res.status(200).json(products)
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/products/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id)
+        res.status(200).json(product)
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/products', async(req, res) => {
+    try {
+
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+        
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({message: error.message})
+    }
 })
 
 mongoose.set("strictQuery", false)
