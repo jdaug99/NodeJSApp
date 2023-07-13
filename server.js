8bdf2fc3caf8f386635631869e7c3a8a619594bf
jdaug99 <jdaug99@gmail.com>
2023-07-12 19:06:01 -0500

    Added productRoute

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const Product = require('./models/productModel')
 const app = express()
+const productRoute = require('./routes/productRoute')
 
 const PORT = process.env.PORT || 3000
 const MONGO_URL = process.env.MONGO_URL
@@ -10,83 +10,10 @@ const MONGO_URL = process.env.MONGO_URL
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+app.use('/api', productRoute)
+
 app.get('/', (req, res) => {
     res.send('Welcome to: BOOSTER ENGINE')
-})
-
-// Read all Products
-app.get('/products', async(req, res) => {
-    try {
-
-        const products = await Product.find({})
-        res.status(200).json(products)
-
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-// Read Product by ID
-app.get('/products/:id', async(req, res) =>{
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id)
-        res.status(200).json(product)
-
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-// Create a Product
-app.post('/products', async(req, res) => {
-    try {
-
-        const product = await Product.create(req.body)
-        res.status(200).json(product);
-        
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).json({message: error.message})
-    }
-})
-
-// Update a Product
-app.put('/products/:id', async(req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body)
-        
-        // If Product not found in db
-        if(!product) {
-            
-            res.status(404).json({message: `No product found with ID ${id}`})
-        }
-        //Else return updated Product
-        const updatedProduct = await Product.findByIdAndUpdate(id, req.body)
-        res.status(200).json(updatedProduct)
-
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-// Delete a Product
-app.delete('/products/:id', async(req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndDelete(id)
-        
-        // If Product not found in db
-        if(!product) {
-            
-            res.status(404).json({message: `No product found with ID ${id}`})
-        }
-        res.status(200).json(product)
-
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
 })
 
 mongoose.set("strictQuery", false)
